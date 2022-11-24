d4ac3f8bc315ee109d7b198b3ae5c61d59aed5b4
Aléctor Alexander <alector.alexandre@gmail.com>
2022-11-24 16:00:46 -0300

    req01

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    new_product = Product(
+        22,
+        "arroz",
+        "BlackSuit",
+        "22-02-02",
+        "12-12-12",
+        "4f6s5d4fs21c5sdr8g4d",
+        "jogue numa panela com carvão dentro, pule amarelinha, conte até 10",
+    )
+    d = "jogue numa panela com carvão dentro, pule amarelinha, conte até 10"
+    assert new_product.id == 22
+    assert new_product.nome_do_produto == "arroz"
+    assert new_product.nome_da_empresa == "BlackSuit"
+    assert new_product.data_de_fabricacao == "22-02-02"
+    assert new_product.data_de_validade == "12-12-12"
+    assert new_product.numero_de_serie == "4f6s5d4fs21c5sdr8g4d"
+    assert new_product.instrucoes_de_armazenamento == d

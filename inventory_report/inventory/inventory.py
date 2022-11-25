1bf345db8442d6511c2ebb203e45895c6c414370
Aléctor Alexander <alector.alexandre@gmail.com>
2022-11-25 18:34:53 -0300

    req07

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import xmltodict
 import json
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
@@ -24,12 +25,26 @@ class Inventory:
         return lista_arquivos
 
     @classmethod
+    def xmlCase(cls, path):
+        with open(path) as fd:
+            xml = xmltodict.parse(fd.read())
+            jSON = json.dumps(xml)
+            response = json.loads(jSON)
+            lista_arquivos = response["dataset"]["record"]
+        return lista_arquivos
+
+    def just_in_the_case(path):
+        if path.endswith(".csv"):
+            lista_arquivos = Inventory.csvCase(path)
+        elif path.endswith(".json"):
+            lista_arquivos = Inventory.jsonCase(path)
+        elif path.endswith(".xml"):
+            lista_arquivos = Inventory.xmlCase(path)
+        return lista_arquivos
+
+    @classmethod
     def import_data(cls, caminho, forma):
-        lista_arquivos = None
-        if caminho.endswith(".csv"):
-            lista_arquivos = Inventory.csvCase(caminho)
-        elif caminho.endswith(".json"):
-            lista_arquivos = Inventory.jsonCase(caminho)
+        lista_arquivos = Inventory.just_in_the_case(caminho)
 
         if forma == "simples":
             return SimpleReport().generate(lista_arquivos)

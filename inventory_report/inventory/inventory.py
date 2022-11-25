25b02a0be25e15ed1bea9122c6576bd6bb5e7663
Aléctor Alexander <alector.alexandre@gmail.com>
2022-11-25 18:14:49 -0300

    req06

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,17 +1,36 @@
 import csv
+import json
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
 
 class Inventory:
     @classmethod
-    def import_data(cls, caminho, forma):
+    def csvCase(cls, path):
         lista_arquivos = []
-        if caminho.endswith('.csv'):
-            with open(caminho, encoding='utf-8') as file:
-                response = csv.DictReader(file, delimiter=",", quotechar='"')
-                for arq in response:
-                    lista_arquivos.append(arq)
+        with open(path, encoding="utf-8") as file:
+            response = csv.DictReader(file, delimiter=",", quotechar='"')
+            for arq in response:
+                lista_arquivos.append(arq)
+        return lista_arquivos
+
+    @classmethod
+    def jsonCase(cls, path):
+        lista_arquivos = []
+        with open(path) as file:
+            response = json.load(file)
+            for arq in response:
+                lista_arquivos.append(arq)
+        return lista_arquivos
+
+    @classmethod
+    def import_data(cls, caminho, forma):
+        lista_arquivos = None
+        if caminho.endswith(".csv"):
+            lista_arquivos = Inventory.csvCase(caminho)
+        elif caminho.endswith(".json"):
+            lista_arquivos = Inventory.jsonCase(caminho)
+
         if forma == "simples":
             return SimpleReport().generate(lista_arquivos)
         if forma == "completo":
